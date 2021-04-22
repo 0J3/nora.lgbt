6eb8f7fd6013590cf5002027208a2081cb794862
0J3 <0J3@wafflepowered.com>
2021-04-22 11:08:54 +0200

    add build (only adding this into the repo so github pages works)

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -4,19 +4,67 @@ import Pfp from '../components/pfp';
 
 import './styles/profile.scss';
 
+import GitHub from '../GitHub.png';
+
+class ProfileLink extends React.Component<{
+	dest: string;
+	img: string;
+	alt?: string;
+}> {
+	render() {
+		return (
+			<Link href={this.props.dest} external>
+				<img
+					src={this.props.img}
+					alt={this.props.alt}
+					style={{
+						maxHeight: '1.5em',
+					}}
+				/>
+			</Link>
+		);
+	}
+}
+
 export default class ProfilePage extends React.Component {
 	render() {
 		return (
 			<div className="profile">
-				<div className="profilePagePfp">
-					<Pfp className="profilePagePfpImg" />
-				</div>
-				<br />
-				<div className="links">
-					<Link href="https://github.com/0j3/" external>
-						Github
-					</Link>
-					<Link to="/">Main</Link>
+				<div className="left">
+					<div className="top">
+						<div className="profilePagePfp">
+							<Pfp className="profilePagePfpImg" />
+						</div>
+					</div>
+					<div
+						className="middle"
+						style={{
+							textAlign: 'left',
+							paddingTop: 20 + 'px',
+						}}
+					>
+						<span
+							className="username"
+							style={{
+								textAlign: 'center',
+								left: '50%',
+								position: 'relative',
+								display: 'block',
+								transform: 'translate(-50%,0)',
+							}}
+						>
+							<span className="user">0J3</span>
+							<span className="tag">#0001</span>
+						</span>
+					</div>
+					<div className="bottom">
+						<div className="links">
+							<ProfileLink
+								dest="https://github.com/0j3/?ref=nora.lgbt/profile"
+								img={GitHub}
+							/>
+						</div>
+					</div>
 				</div>
 			</div>
 		);
--- src/pages/goto.tsx
+++ src/pages/goto.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 export default class ProfilePage extends React.Component {
 	render() {
+		document.location.replace(
+			document.location.href.replace(
+				`${document.location.protocol}//${document.location.host}/goto/`,
+				''
+			)
+		);
 		return <div>Redirecting...</div>;
 	}
 }
