66dd2b2fa848d2907a28d3642d9dbc2f4458b61c
0J3 <0J3@wafflepowered.com>
2021-05-05 18:50:37 +0200

    a

---
+++ src/pages/Soon.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Pfp from '../components/pfp';
+import Link from '../components/Link';
+
+import styles from './styles/Index.module.scss';
+import './styles/Index.scss';
+import './styles/Logo.scss';
+
+export default class e404 extends React.Component {
+	render() {
+		return (
+			<div>
+				<Pfp />
+				{/* <Counter /> */}
+				<p className={`ComingSoon ${styles.ComingSoonText}`}>
+					This{' '}
+					{document.location.hash
+						? document.location.hash.replace('#', '')
+						: 'project'}{' '}
+					is coming soon.
+				</p>
+				<span></span>
+			</div>
+		);
+	}
+}
