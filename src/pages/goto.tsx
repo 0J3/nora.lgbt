ddca56fb870d0a95de8deec41d9dafeebf9ac6da
0J3 <0J3@wafflepowered.com>
2021-05-08 16:53:56 +0200

    le fix

--- src/pages/goto.tsx
+++ src/pages/goto.tsx
@@ -58,7 +58,7 @@ export default class Goto extends React.Component {
 			);
 		if (
 			!(
-				t.toLowerCase().startsWith('https://') &&
+				t.toLowerCase().startsWith('https://') ||
 				t.toLowerCase().startsWith('http://')
 			)
 		)
