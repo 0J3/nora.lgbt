0f07a4f0ac9f5764af050a96e845993aac1ff98f
0J3 <0J3@wafflepowered.com>
2021-06-14 16:31:50 +0200

    a

---
+++ express.js
@@ -0,0 +1,3 @@
+// EXPRESS
+// This file handles adding embeds when serving the files - You can serve static files, but these will have a weird, unwanted embed.
+// Copyright (c) 2021 0J3. All Rights Reserved.
