0924edd6f0ef6fa7438cb24a35b6b29d4d35b021
0J3 <0J3@wafflepowered.com>
2021-07-20 20:52:17 +0200

    fix v2

--- src/routes/about/AboutSection.tsx
+++ src/routes/about/AboutSection.tsx
@@ -7,7 +7,7 @@ export default (Class: (a: string) => string) => {
   return (
     <div className={Class('section aboutSection')}>
       <img
-        src='https://cdn.discordapp.com/avatars/596425713347723269/f306c7f869d2ddff49243110bafb9f6e.webp?size=128'
+        src='https://cdn.discordapp.com/avatars/596425713347723269/f306c7f869d2ddff49243110bafb9f6e.webp?size=512'
         alt='Profile Picture'
         className={Class('pfpImg')}
         style={{
