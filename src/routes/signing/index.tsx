2c880fae71b414f5108db1ce2020a2e74f7deaa8
0J3 <git@wafflepowered.com>
2021-08-05 05:25:33 +0200

    add mit keyserver lol

--- src/routes/signing/index.tsx
+++ src/routes/signing/index.tsx
@@ -24,7 +24,32 @@ export default class Signing extends Component<{}, {}> {
         <h3>PGP</h3>
         <p>
           You can find my PGP key{' '}
-          <Link href='/goto/gh/0J3/nora.lgbt/blob/v3/pubkey'>here</Link>.
+          <Link href='/goto/gh/0J3/nora.lgbt/blob/v3/pubkey'>here</Link>.<br />
+          You can also find my key in the MIT Keyserver (
+          <a href='https://pgp.mit.edu/' target='_blank'>
+            https://pgp.mit.edu/
+          </a>
+          ) with the KeyID{' '}
+          <a
+            href='https://pgp.mit.edu/pks/lookup?search=0x6EB0955C870485A0&op=index'
+            target='_blank'
+          >
+            <code>0x6eb0955c870485a0</code>
+          </a>
+          , or by clicking{' '}
+          <a
+            href='https://pgp.mit.edu/pks/lookup?op=vindex&search=0x6EB0955C870485A0'
+            target='_blank'
+          >
+            here
+          </a>{' '}
+          or{' '}
+          <a
+            href='https://pgp.mit.edu/pks/lookup?op=get&search=0x6EB0955C870485A0'
+            target='_blank'
+          >
+            here
+          </a>
         </p>
         <h3>Code Signing Certificate</h3>
         <p>
